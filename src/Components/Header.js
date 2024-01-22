483f9fdace924b7e37d6ed39a8000544ab8cd3d0
Jason <jasonsnod@gmail.com>
2024-01-22 10:48:12 -0500

    JS: changed brand name to 16-bit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,7 +3,7 @@ function Header() {
     <div className="nav-bar">
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href="#">
-          Navbar
+          16-Bit
         </a>
         <button
           class="navbar-toggler"
