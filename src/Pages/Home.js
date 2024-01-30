23427b4035828c6e89ce71b3ff31e089ebad4b29
Jason <jasonsnod@gmail.com>
2024-01-30 14:15:53 -0500

    JS: Imported components into Home component

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,21 @@
-import Header from '../Components/Header.js';
+import Header from "../Components/Header.js";
+import LandingImage from "../Components/Landing-Image.js";
+import ExampleWebsites from "../Components/Example-Websites.js";
+import About from "../Components/About.js";
+import ContactUs from "../Components/Contact-Us.js";
+import Footer from "../Components/Footer.js";
 
 function Home() {
-    return (
-        <Header />
-    );
+  return (
+    <>
+      <Header />
+      <LandingImage />
+      <ExampleWebsites />
+      <About />
+      <ContactUs />
+      <Footer />
+    </>
+  );
 }
 
 export default Home;
